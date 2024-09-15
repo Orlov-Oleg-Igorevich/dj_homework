75f2bf19e92e1764613a83169dd834b1feaf2294
Oleg <oleg_orlov_2005@mail.ru>
2024-09-15 13:36:59 +0300

    make task relationship

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') == True:
+                count += 1
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+        if count > 1:
+            raise ValidationError('Тут всегда ошибка')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
+
 
 
 @admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+    list_display = ['title', 'published_at']
+
+@admin.register(Tag)
+class TagModel(admin.ModelAdmin):
+    list_display = ['name']
+
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Article(models.Model):
@@ -11,6 +12,27 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+class Scope(models.Model):
+    tag = models.ForeignKey('Tag', related_name='scopes', on_delete=models.PROTECT)
+    article = models.ForeignKey('Article', related_name='scopes', on_delete=models.PROTECT)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('article',), condition=Q(is_main=True), name='one_main_per_user')
+        ]
+        ordering = ['-is_main']
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
